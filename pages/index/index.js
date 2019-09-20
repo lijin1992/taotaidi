205bbf439fdd6d077c688b51ef43074c2feafbb5
lijin1992 <903496672@qq.com>
2019-09-20 15:48:40 +0800

    修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,5 +9,21 @@ Page({
       sourceValue:e.detail.value,
       disabled:!!this.data.sourceValue
     })  
+  },
+  startChange(e){
+    this.setData({
+      changeValue:`襄遇二阳老襄阳手工锅巴麻辣特产小吃网红包装好吃休闲食品小零食 【包邮】
+      【在售价】8.80元
+      【券后价】5.80元
+      【下单链接】https://m.tb.cn/h.eNUSkZV 
+      ----------------- 
+      复制这条信息，$gCFgYNnLlOM$，到【手机淘宝】即可查看`
+    })
+    wx.setClipboardData({
+      data: this.data.changeValue,
+      success (res) {
+       
+      }
+    })
   }
 })
